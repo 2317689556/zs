c64018f3ac708ce0cb1ad74c96be51a3b14e1f99
shenshutong <13780892097@163.com>
2020-10-08 11:20:00 +0800

    1

--- src/main/java/spring/controller/UserController.java
+++ src/main/java/spring/controller/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
             return "index";
         }else{
             return "login";
-            666郝佳乐ffffffff
+            666郝佳乐ffffffffttttt
         }
     }*/
 
